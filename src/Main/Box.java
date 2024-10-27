a7847de9a18cedf9dd4a26eae12e953af8e5e326
aryno <arynovalmaz7@gmail.com>
2024-10-27 10:03:27 +0100

    final

---
+++ src/Main/Box.java
@@ -0,0 +1,15 @@
+public class Box {
+    private Object object;
+
+    public Box(Object object) {
+        this.object = object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+}
